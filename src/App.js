0076bd755aab9c4808ed1716f1076a68c9d2162c
kubesqrt <96239298+kubesqrt@users.noreply.github.com>
2022-08-09 23:07:13 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import React from "react"
+import ReactDOM from "react-dom"
+
+const page = (
+
+  <div>
+    <img src="./spidey.png" />
+  </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
+
